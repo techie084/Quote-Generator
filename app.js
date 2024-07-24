322fb95ef6031bd3e1adf6062356b932cd3f05d8
Wisdom Matthew <wisdommatthew08@gmail.com>
2024-07-24 01:27:33 +0100

    changes

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const quotes = [
   },
   {
     quote: `"If you look at what you have in life, you will always have more.
-     If you look at what you don't have in life, you'll never have enough."`,
+    If you look at what you don't have in life, you'll never have enough."`,
     person: ` Oprah Winfrey `,
   },
   {
